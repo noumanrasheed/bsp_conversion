fef0f469ae2ab8354c0a659e6983d369ef6b5975
noumanrasheed <noumanrasheed.ibd@gmail.com>
2024-11-04 23:47:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,33 +11,33 @@ from bs4 import BeautifulSoup
 import shutil
 
 # Google AdSense ad code
-adsense_url = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-GA_AdSense = """
- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
- <ins class="adsbygoogle"
-     style=<your_ad_style>
-     data-ad-client=<your_client_id>
-     data-ad-slot=<your_ad_slot>
-     data-ad-format=<your_ad_format>
-     data-full-width-responsive="true"></ins>
- <script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
- </script>
-"""
+# adsense_url = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+# GA_AdSense = """
+#  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+#  <ins class="adsbygoogle"
+#      style=<your_ad_style>
+#      data-ad-client=<your_client_id>
+#      data-ad-slot=<your_ad_slot>
+#      data-ad-format=<your_ad_format>
+#      data-full-width-responsive="true"></ins>
+#  <script>
+#      (adsbygoogle = window.adsbygoogle || []).push({});
+#  </script>
+# """
 
-# Insert the script in the head tag of the static template inside your virtual
-index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-logging.info(f'editing {index_path}')
-soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-if not soup.find('script', src=adsense_url): 
-   bck_index = index_path.with_suffix('.bck')
-   if bck_index.exists():
-       shutil.copy(bck_index, index_path)  
-   else:
-       shutil.copy(index_path, bck_index)  
-   html = str(soup)
-   new_html = html.replace('<head>', '<head>\n' + GA_AdSense)
-   index_path.write_text(new_html)
+# # Insert the script in the head tag of the static template inside your virtual
+# index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+# logging.info(f'editing {index_path}')
+# soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+# if not soup.find('script', src=adsense_url): 
+#    bck_index = index_path.with_suffix('.bck')
+#    if bck_index.exists():
+#        shutil.copy(bck_index, index_path)  
+#    else:
+#        shutil.copy(index_path, bck_index)  
+#    html = str(soup)
+#    new_html = html.replace('<head>', '<head>\n' + GA_AdSense)
+#    index_path.write_text(new_html)
 
 # Render the ad in an iframe
 # components.iframe(adsense_ad, width=728, height=90)
